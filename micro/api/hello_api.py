9f7d34079637f0b23d6a92893b2a6e1218ca8732
Iain Hunter <iain.hunter@thinkwhere.com>
2017-04-18 23:23:35 +0100

    Document sample APIs

--- micro/api/hello_api.py
+++ micro/api/hello_api.py
@@ -4,12 +4,47 @@ from flask_restful import Resource
 class HelloAPI(Resource):
 
     def get(self):
-        """ Greets the world """
+        """
+        Greets GeoPython conf
+        ---
+        tags:
+          - hello
+        produces:
+          - application/json
+        responses:
+            200:
+                description: Says hello
+            500:
+                description: Internal Server Error
+        """
         return {"hello": "GeoPython 17"}, 200
 
 
 class CalcAPI(Resource):
 
     def get(self, num1: int, num2: int):
-        """ Multiplies two numbers together """
+        """
+        Simple calculator that multiplies 2 numbers
+        ---
+        tags:
+          - calculator
+        produces:
+          - application/json
+        parameters:
+            - name: num1
+              in: path
+              description: First number to multiply
+              required: true
+              type: integer
+            - name: num2
+              in: path
+              description: Second number to multiply
+              required: true
+              type: integer
+        responses:
+            200:
+                description: Answer provided
+            500:
+                description: Internal Server Error
+        """
         return {"answer": num1 * num2}, 200
