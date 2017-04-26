c35f82855d342fc33ab8f10151105c6194a3480d
Iain Hunter <iain.hunter@thinkwhere.com>
2017-04-26 23:35:51 +0100

    Remove CORS

--- micro/__init__.py
+++ micro/__init__.py
@@ -1,7 +1,6 @@
 import logging
 import os
 from flask import Flask
-from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
